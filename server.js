9fe19b31d143d4ba4e6427409923fcfbb021f14c
Manikandan Rajagopal <manir1811@gmail.com>
2019-06-11 20:02:05 +0300

    Login and register setup

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+// Require env file
+require("dotenv").config();
+
+const users = require("./routes/api/users");
+
+const app = express();
+
+// Body Parser setup
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB Config
+const db = process.env.mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch(err => console.log(err));
+
+// Passport setup
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+app.get("/", (req, res) => {
+  res.send("Root page");
+});
+
+// Use Routes
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
